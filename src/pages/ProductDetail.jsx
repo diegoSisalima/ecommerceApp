43fc3ec9aa5b8f098d4535ddd656f32bd023dd41
kristian-11 <cquiceno609@gmail.com>
2022-10-04 02:13:14 -0500

    style productdetail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -14,21 +14,71 @@ const ProductDetail = () => {
   );
 
   return (
-    <div>
+    <div className="product">
       <h1>Estas en Product Detail</h1>
-      <p>
-        Mostrando producto de id: <b>{id}</b>
-      </p>
-      <h4>{productDetail.title}</h4>
+      <div className="productInfo">
+        <div className="col">
+          <div className="images-gallery">
+            <div className="gallery">
+              <div className="button-gallery left">
+                <button>
+                  ◀
+                </button>
+              </div>
+              <ul className="producto-img-pri">
+                <li>
+                  <img src={productDetail.productImgs[0]} alt="" />
+                </li>
+              </ul>
+              <div className="button-gallery rigth">
+                <button>
+                  ▶
+                </button>
+              </div>
+            </div>
+            <ul className="product-gallery">
+              <li className="selected"><img src={productDetail.productImgs[0]} alt="" /></li>
+              <li className="selected"><img src={productDetail.productImgs[1]} alt="" /></li>
+              <li className="selected"><img src={productDetail.productImgs[2]} alt="" /></li>
+            </ul>
+          </div>
+        </div>
+        <div className="col">
+          <h2>{productDetail.title}</h2>
+          <div className="productData">
+            <p className="productDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nemo ex obcaecati delectus ducimus quaerat quod doloribus fuga ipsa tempore. Quod aperiam natus modi doloribus eos fugit dignissimos ipsam eum?
+            </p>
+            <div className="product-options">
+              <ul>
+                <li className="price">Price</li>
+                <li className="price-num">{productDetail.price}</li>
+              </ul>
+              <div className="quantity">
+                <div className="label">Quantity</div>
+                <div className="flex">
+                  <button disabled>
+                   <i className="icon-minus">-</i>
+                  </button>
+                  <div className="value">1</div>
+                  <button>
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+            <button className="productButton">Add to cart</button>
+          </div>
+        </div>
+      </div>
       <ul>
         {relatedProducts.map((products) => (
-          <li>
+          <li key={products.id}>
+            {console.log(products)}
             <Link to={`/product/${products.id}`}>{products.title}</Link>
           </li>
         ))}
       </ul>
-      <img src={productDetail.productImgs[0]} alt="" />
-      <h4>{productDetail.price}</h4>
     </div>
   );
 };
